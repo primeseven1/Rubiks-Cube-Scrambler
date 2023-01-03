b9e4737bc6dd0ea0cedb73311ce327c6c8422ed5
Chris <90427756+primeseven1@users.noreply.github.com>
2023-01-03 01:50:29 -0800

    Update cube.c

--- cube.c
+++ cube.c
@@ -5,7 +5,7 @@ int valid(const char* scramble, const int i, const char move)
 	// Checks if the current move is the same as the previous one
 	if (i && scramble[i - 1] == move) return 0;
 
-	// Removes things like "D, U2, D"
+	// Removes things like "D U2 D"
 	if (i > 1 && move == scramble[i - 2])
 	{
 		if ((move == 'U' && scramble[i - 1] == 'D') || (move == 'D' && scramble[i - 1] == 'U') ||
