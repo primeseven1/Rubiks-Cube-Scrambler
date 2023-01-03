e2c3bf7235b7aa637617788fb1d666ff7f0131cb
Chris <90427756+primeseven1@users.noreply.github.com>
2023-01-03 01:29:25 -0800

    Add files via upload

--- cube.c
+++ cube.c
@@ -8,9 +8,9 @@ int valid(const char* scramble, const int i, const char move)
 	// Removes things like "D, U2, D"
 	if (i > 1 && move == scramble[i - 2])
 	{
-		if ((move == 'U') && (scramble[-1] == 'D') || (move == 'D') && (scramble[-1] == 'U') ||
-			(move == 'R') && (scramble[-1] == 'L') || (move == 'L') && (scramble[-1] == 'R') ||
-			(move == 'F') && (scramble[-1] == 'B') || (move == 'B') && (scramble[-1] == 'F')) return 0;
+		if ((move == 'U' && scramble[i - 1] == 'D') || (move == 'D' && scramble[i - 1] == 'U') ||
+			(move == 'R' && scramble[i - 1] == 'L') || (move == 'L' && scramble[i - 1] == 'R') ||
+			(move == 'F' && scramble[i - 1] == 'B') || (move == 'B' && scramble[i - 1] == 'F')) return 0;
 	}
 
 	return 1;
