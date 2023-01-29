5ec2774808299e50609ea05be9417d48a1f3f01e
Chris <90427756+primeseven1@users.noreply.github.com>
2023-01-29 00:25:50 -0800

    Delete scrambleGenerator.h

--- scrambleGenerator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <time.h>
-#include <stdlib.h>
-
-char** genScramble(const unsigned int scrambleLength);
-int valid(const char** scramble, const int i, const char move);
-void freeScramble(char** scramble, const unsigned int scrambleLength);
