23c8be6409666e8d3be9d8a2dfff4875b31540fd
Chris <90427756+primeseven1@users.noreply.github.com>
2023-01-29 00:57:49 -0800

    Delete src directory

--- src/C/errorWindow.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-void loadFileError(const char* filePath);
-int errorWindow(LPCSTR errorType, LPCSTR errorInfo);
--- src/C/scrambleGenerator.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-char** genScramble(const unsigned int scrambleLength);
-int valid(const char** scramble, const unsigned int i, const char move);
-void freeScramble(char** scramble, const unsigned int scrambleLength);
