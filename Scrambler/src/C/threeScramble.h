014c7ff7175766fa3c34e739dc847c4c8da06d97
Chris <90427756+primeseven1@users.noreply.github.com>
2023-02-07 01:49:04 -0800

    Delete Scrambler directory

--- Scrambler/src/C/threeScramble.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// The genThreeScramble function is also able to generate 2x2 scrambles, the scramble length determines what it generates
-#define genTwoScramble genThreeScramble
-
-char** genThreeScramble(unsigned int scrambleLength);
