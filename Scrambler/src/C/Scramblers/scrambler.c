ff97b22609efc419269f6fb0224d22c0b28b3b4f
Chris <90427756+primeseven1@users.noreply.github.com>
2023-02-19 16:24:10 -0800

    Better code

--- Scrambler/src/C/Scramblers/scrambler.c
+++ Scrambler/src/C/Scramblers/scrambler.c
@@ -30,60 +30,26 @@ char** genScramble(const Puzzle puzzle)
 {
 	char** scramble = NULL;
 
-	// The last element are scramble lengths
-	unsigned int puzzleInfo[9][3] = { { THREE_BY_THREE, THREE_BY_THREE_MODIFIERS, rand() % (29 - 24) + 24 },
-									{ TWO_BY_TWO, TWO_BY_TWO_MODIFIERS, rand() % (12 - 9) + 9},
-									{ FOUR_BY_FOUR, FOUR_BY_FOUR_MODIFIERS, rand() % (49 - 42) + 42},
-									{ FIVE_BY_FIVE, FIVE_BY_FIVE_MODIFIERS, rand() % (63 - 55) + 55},
-									{ SIX_BY_SIX, SIX_BY_SIX_MODIFIERS, rand() % (85 - 78) + 78},
-									{ SEVEN_BY_SEVEN, SEVEN_BY_SEVEN_MODIFIERS, rand() % (103 - 91) + 91},
-									{ SKEWB, SKEWB_MODIFIERS, rand() % (10 - 7) + 7},
-									{ PYRAMINX, PYRAMINX_MODIFIERS, rand() % (15 - 10) + 10},
-									{ MEGAMINX, MEGAMINX_MODIFIERS, 77}
-								  /*{CLOCK, CLOCK_MODIFIERS, 19}*/ };
+	// The last third element of the arrays are scramble lengths
+	// The last element of the arrays are function pointers
+	unsigned long long puzzleInfo[9][4] = { 
+		{ THREE_BY_THREE, THREE_BY_THREE_MODIFIERS, rand() % (29 - 24) + 24, genThreeScramble },
+		{ TWO_BY_TWO, TWO_BY_TWO_MODIFIERS, rand() % (12 - 9) + 9, genTwoScramble },
+		{ FOUR_BY_FOUR, FOUR_BY_FOUR_MODIFIERS, rand() % (49 - 42) + 42, genFourScramble },
+		{ FIVE_BY_FIVE, FIVE_BY_FIVE_MODIFIERS, rand() % (63 - 55) + 55, genFiveScramble },
+		{ SIX_BY_SIX, SIX_BY_SIX_MODIFIERS, rand() % (85 - 78) + 78, genSixScramble },
+		{ SEVEN_BY_SEVEN, SEVEN_BY_SEVEN_MODIFIERS, rand() % (103 - 91) + 91, genSevenScramble },
+		{ SKEWB, SKEWB_MODIFIERS, rand() % (10 - 7) + 7, genSkewbScramble },
+		{ PYRAMINX, PYRAMINX_MODIFIERS, rand() % (15 - 10) + 10, genPyraminxScramble },
+		{ MEGAMINX, MEGAMINX_MODIFIERS, 77, genMegaminxScramble }
+		};
 
 	unsigned int i = 0;
 	while (puzzle != puzzleInfo[i][0]) i++;
 	scramble = allocator(puzzleInfo[i][1], puzzleInfo[i][2]);
 
-	switch (puzzle)
-	{
-	case THREE_BY_THREE:
-		genThreeScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case TWO_BY_TWO:
-		genTwoScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case FOUR_BY_FOUR:
-		genFourScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case FIVE_BY_FIVE:
-		genFiveScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case SIX_BY_SIX:
-		genSixScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case SEVEN_BY_SEVEN:
-		genSevenScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case SKEWB:
-		genSkewbScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case PYRAMINX:
-		genPyraminxScramble(scramble, puzzleInfo[i][2]);
-		break;
-
-	case MEGAMINX:
-		genMegaminxScramble(scramble, puzzleInfo[i][2]);
-		break;
-	}
+	void(*scrambleGen)(char** scramble, unsigned int scrambleLength) = puzzleInfo[i][3];
+	scrambleGen(scramble, puzzleInfo[i][2]);
 
 	return scramble;
 }
