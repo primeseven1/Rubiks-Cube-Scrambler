3af64276b7ea1846c59e74afebad2f547bf5576b
Chris <90427756+primeseven1@users.noreply.github.com>
2023-02-19 18:04:59 -0800

    Add files via upload

--- Scrambler/src/C++/Window/window.cpp
+++ Scrambler/src/C++/Window/window.cpp
@@ -3,20 +3,14 @@
 #include "../ScrambleText/scrambleText.h"
 
 Window::Window(const unsigned int width, const unsigned int height, const char* title, sf::Uint32 style)
+	: _m_keyBindings {
+		{ sf::Keyboard::Enter, []() { ScrambleText::getInstance().generateScramble(); } }, { sf::Keyboard::Num2, []() { ScrambleText::getInstance().setPuzzle(TWO_BY_TWO); } },
+		{ sf::Keyboard::Num3, []() { ScrambleText::getInstance().setPuzzle(THREE_BY_THREE); } }, { sf::Keyboard::Num4, []() { ScrambleText::getInstance().setPuzzle(FOUR_BY_FOUR); } },
+		{ sf::Keyboard::Num5, []() { ScrambleText::getInstance().setPuzzle(FIVE_BY_FIVE); } }, { sf::Keyboard::Num6, []() { ScrambleText::getInstance().setPuzzle(SIX_BY_SIX); } },
+		{ sf::Keyboard::Num7, []() { ScrambleText::getInstance().setPuzzle(SEVEN_BY_SEVEN); } }, { sf::Keyboard::S, []() { ScrambleText::getInstance().setPuzzle(SKEWB); } },
+		{ sf::Keyboard::P, []() { ScrambleText::getInstance().setPuzzle(PYRAMINX); } }, { sf::Keyboard::M, []() { ScrambleText::getInstance().setPuzzle(MEGAMINX); } } 
+		}
 {
-	_m_keyBindings = {
-	{ sf::Keyboard::Enter, []() { ScrambleText::getInstance().generateScramble(); } },
-	{ sf::Keyboard::Num2, []() { ScrambleText::getInstance().setPuzzle(TWO_BY_TWO); } },
-	{ sf::Keyboard::Num3, []() { ScrambleText::getInstance().setPuzzle(THREE_BY_THREE); } },
-	{ sf::Keyboard::Num4, []() { ScrambleText::getInstance().setPuzzle(FOUR_BY_FOUR); } },
-	{ sf::Keyboard::Num5, []() { ScrambleText::getInstance().setPuzzle(FIVE_BY_FIVE); } },
-	{ sf::Keyboard::Num6, []() { ScrambleText::getInstance().setPuzzle(SIX_BY_SIX); } },
-	{ sf::Keyboard::Num7, []() { ScrambleText::getInstance().setPuzzle(SEVEN_BY_SEVEN); } },
-	{ sf::Keyboard::S, []() { ScrambleText::getInstance().setPuzzle(SKEWB); } },
-	{ sf::Keyboard::P, []() { ScrambleText::getInstance().setPuzzle(PYRAMINX); } },
-	{ sf::Keyboard::M, []() { ScrambleText::getInstance().setPuzzle(MEGAMINX); } }
-	};
-
 	this->create(sf::VideoMode(width, height), title, style);
 	this->setFramerateLimit(30); // Reduces CPU usage by a lot
 	this->_eventHandler();
