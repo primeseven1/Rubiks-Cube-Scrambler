ac28a56843e0c27f7c1de614025ef4522c5f96d9
Chris <90427756+primeseven1@users.noreply.github.com>
2023-02-24 01:32:27 -0800

    Update scrambleText.cpp

--- Scrambler/src/C++/ScrambleText/scrambleText.cpp
+++ Scrambler/src/C++/ScrambleText/scrambleText.cpp
@@ -34,10 +34,10 @@ void ScrambleText::setPuzzle(Puzzle puzzle) { _m_puzzle = puzzle; }
 void ScrambleText::generateScramble()
 {
 	char** scramble = genScramble(_m_puzzle);
-	this->_setScramble(scramble);
+	this->setScramble(scramble);
 }
 
-void ScrambleText::_setScramble(char** scramble)
+void ScrambleText::setScramble(char** scramble)
 {
 	// The +1 includes the move that's not a modifer, since that needs to be displayed too
 	unsigned int modifiers = _m_puzzle < FOUR_BY_FOUR ? 1 + 1 : _m_puzzle < SIX_BY_SIX ? 2 + 1 : 3 + 1;
